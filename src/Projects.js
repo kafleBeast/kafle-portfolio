8427b3493297b4cd99fae2dbada69eb4d75f0ee3
Abhishek Kafle <68716736+kafleBeast@users.noreply.github.com>
2022-04-17 14:53:54 +0900

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -9,20 +9,6 @@ import seniorDesign from './images/seniorDesign.gif'
 
 const PROJECTS = [
   {
-    image: greprep,
-    name: "GRE Vocab Preparation",
-    technologyUsed: "ReactJs, Javascript",
-    description: "A platform to learn vocabularies with the primary goal of scoring 160+ in GRE Verbal section. The website includes features to customize your personal list of words or to learn from the prewritten SETS. Users can also learn using flashcards or Quizzes.",
-    url: "https://focused-wiles-f203aa.netlify.app",
-  },
-  {
-    image: finfo,
-    name: "FINFO",
-    technologyUsed: "ReactJs, Javascript, R",
-    description: "A web application that creates stock price forecast based on sentiment analysis by acquiring twitter data. Other features include: Company news, key financial data, and price in real-time, and screener",
-    url: "https://wizardly-wozniak-1a82a7.netlify.app/",
-  },
-  {
     image: research,
     name: "Optimal Control of Several Motion Models",
     technologyUsed: "Python for Simulation",
@@ -35,7 +21,21 @@ const PROJECTS = [
     technologyUsed: "OpenCV, Python, Arduino, CAD, 3D-printing",
     description: "In this year-long collaborative project, we designed and manufactured an electromechanical system consisting of a motion capture system using high FPS cameras, a data processing unit using the OpenCV library, and a mechanical 2-D response platform operated using microcontrollers. The response platform, which is a dartboard connected via pulleys to the stepper motors, intercepts the incoming dart by calculating its trajectory in real-time. The motivation to design such a system came from the emerging need to intercept and remove orbital debris, also known as “space junk”, which threatens human spaceflight and other robotic missions.",
     url: "https://docs.google.com/document/d/119gi_KJ1Ycaml0ALVMv3zG3diBU94sUiBAftzB6Bmlk/edit?usp=sharing",
-  }
+  },
+  {
+    image: finfo,
+    name: "FINFO",
+    technologyUsed: "ReactJs, Javascript, R",
+    description: "A web application that creates stock price forecast based on sentiment analysis by acquiring twitter data. Other features include: Company news, key financial data, and price in real-time, and screener",
+    url: "https://wizardly-wozniak-1a82a7.netlify.app/",
+  },
+  {
+    image: greprep,
+    name: "GRE Vocab Preparation",
+    technologyUsed: "ReactJs, Javascript",
+    description: "A platform to learn vocabularies with the primary goal of scoring 160+ in GRE Verbal section. The website includes features to customize your personal list of words or to learn from the prewritten SETS. Users can also learn using flashcards or Quizzes.",
+    url: "https://focused-wiles-f203aa.netlify.app",
+  },
 ];
 
 const Projects = () =>{
@@ -44,8 +44,8 @@ const Projects = () =>{
   return (
     <Container fluid className="project-section">
       <Container>
-        <h1 className="project-heading" >
-          My Recent <strong className="purple">Works </strong>
+        <h1 className="project-heading">
+          My Recent <strong className="purple"> Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
